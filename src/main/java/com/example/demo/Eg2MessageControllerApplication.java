7ba2ff6072c3a60067372ad56acc6391c190a28c
HarithKavish <harithkavish40@gmail.com>
2023-02-21 23:21:37 +0530

    commit

---
+++ src/main/java/com/example/demo/Eg2MessageControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eg2MessageControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eg2MessageControllerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/messageResponse.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class messageResponse {
+	private String message;
+	public messageResponse(String message){
+		this.message=message;
+	}
+	public String getMessage() {
+		return message;
+	}
+}
